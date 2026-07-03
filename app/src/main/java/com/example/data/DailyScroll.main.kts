e707aba7d571d96294e1101b05a0002d54e09f4b
0xDefult <onesattyam@gmail.com>
2026-07-03 05:39:36 +0530

    define dailyscroll entity and implement getdatabase singleton

--- app/src/main/java/com/example/data/DailyScroll.main.kts
+++ app/src/main/java/com/example/data/DailyScroll.main.kts
@@ -1,2 +1,14 @@
-#!/usr/bin/env kotlin
+package com.example.data
 
+
+import androidx.room.Entity
+import androidx.room.PrimryKey
+
+
+@Entity(tableName = "daily_scrolls")
+data class DailyScroll(
+    @PrimaryKey val data: String, //dates
+    val count: Int,
+    val timeSpentSeconds: Long = 0,
+    val averageTimePerReel: Float = 18f // seconds
+)
